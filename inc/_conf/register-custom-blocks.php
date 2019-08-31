88751daecfdc770b5d947b1d6fad242e33a72671
Jaakko Saarenketo <jaakko@favor.org>
2019-08-31 16:54:58 +0300

    Nostoblokki is better with 100% wide fields and Preview

--- inc/_conf/register-custom-blocks.php
+++ inc/_conf/register-custom-blocks.php
@@ -23,5 +23,26 @@ function register_custom_blocks() {
         //   'icon'              => array('background' => 'lavender', 'src' => 'nametag'),
         // ));
 
+        acf_register_block_type(array(
+          'name'              => 'nostoblokki',
+          'title'             => __('Nostoblokki'),
+          // 'description'       => __('A custom testimonial block.'),
+          'render_template'   => 'partials/content/nosto.php',
+          'category'          => 'formatting',
+          'mode'              => 'preview',
+          'icon'              => array('background' => 'lavender', 'src' => 'nametag'),
+      ));
+        acf_register_block(array(
+          'name'              => 'sivulista',
+          'title'             => __('Sivulista'),
+          'description'       => __('Luo 3 uusinta uutista, listaa kategorian mukaan sisältöjä…'),
+          'render_template'   => 'partials/content/sivulista.php',
+          'category'          => 'formatting',
+          // 'icon'              => 'admin-comments',
+          'mode'              => 'edit',
+          'icon'              => array('background' => 'linen', 'src' => 'excerpt-view'),
+          //'keywords'          => array( 'testimonial', 'quote' ),
+        ));
+
     }
 }

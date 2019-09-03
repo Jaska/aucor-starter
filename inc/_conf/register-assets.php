a647b2c8de9477309e061b34558a3b1d777072d7
Jaakko Saarenketo <jaakko@favor.org>
2019-09-03 09:18:59 +0300

    Helpful favicon template #cherry

--- inc/_conf/register-assets.php
+++ inc/_conf/register-assets.php
@@ -151,9 +151,19 @@ add_action('wp_footer', function() {
  *
  * Add favicons' <link> and <meta> tags here
  */
-function aucor_starter_favicons() {
-
+function starter_favicons() {
+  // Use https://realfavicongenerator.net and insert all files in dist/favicon
+  $x = get_template_directory_uri().'/dist/favicon';
+  ?>
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $x; ?>/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $x; ?>/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $x; ?>/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo $x; ?>/site.webmanifest">
+  <link rel="mask-icon" href="<?php echo $x; ?>/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+  <?php
 }
-add_action('wp_head',    'aucor_starter_favicons');
-add_action('admin_head', 'aucor_starter_favicons');
-add_action('login_head', 'aucor_starter_favicons');
+add_action('wp_head',    'starter_favicons');
+add_action('admin_head', 'starter_favicons');
+add_action('login_head', 'starter_favicons');

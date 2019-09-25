643199cdd8ab47d2f0caed3a6d45e4eff277b955
Jaakko Saarenketo <jaakko@favor.org>
2019-09-25 11:48:06 +0300

    removes sitemanifest

--- inc/_conf/register-assets.php
+++ inc/_conf/register-assets.php
@@ -158,7 +158,6 @@ function starter_favicons() {
   <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $x; ?>/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $x; ?>/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $x; ?>/favicon-16x16.png">
-  <link rel="manifest" href="<?php echo $x; ?>/site.webmanifest">
   <link rel="mask-icon" href="<?php echo $x; ?>/safari-pinned-tab.svg" color="#5bbad5">
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="theme-color" content="#ffffff">

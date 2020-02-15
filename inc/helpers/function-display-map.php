ace94a3805207606d004ccb33151a05fa3f630e6
Jaakko Saarenketo <jaakko@favor.org>
2020-02-15 02:01:17 +0200

    #cherry removes ghostcode from function

--- inc/helpers/function-display-map.php
+++ inc/helpers/function-display-map.php
@@ -1,7 +1,7 @@
 <?php
 
 function display_map(){
-  $map = '<div class="map__wrapper"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d843246.3570092141!2d26.252035168461664!3d65.61937082178433!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x442a32c3beffaab1%3A0x2c453b0edbcc3b95!2sHotelli%20Iso-Sy%C3%B6te!5e0!3m2!1sfi!2sfi!4v1567587292242!5m2!1sfi!2sfi" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe></div>';
+  $map = '<div class="map__wrapper"><iframe src="" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe></div>';
 
   return $map;
 }

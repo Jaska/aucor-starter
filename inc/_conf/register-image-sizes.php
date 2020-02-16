78f96a3ac374db07c4cf86290c7368a0df2ae3ae
Jaakko Saarenketo <jaakko@favor.org>
2020-02-16 10:04:20 +0200

    makes hero_md same aspect ratio as xl cropping easier

--- inc/_conf/register-image-sizes.php
+++ inc/_conf/register-image-sizes.php
@@ -40,7 +40,7 @@ add_action('after_setup_theme', function() {
   // custom image sizes
   add_image_size('lazyload',   14,    8,  true); // (default starter) small ~16:9
   add_image_size('hero_xl',  2000,  750, true);  // (default starter) hero @1,33x 3:1
-  add_image_size('hero_md',  1500,  500, true);  // (default starter) hero @1x 3:1
+  add_image_size('hero_md',  1504,  564, true);  // (default starter) hero @1x 3:1 (old: 1500/500)
   add_image_size('hero_sm',   800,  500, true);  // (default starter) hero mobile 8:5
   add_image_size('wide',     1216, 2432, false); // (default starter) gutenberg wide, relational to large
   add_image_size('wide_xl',  1824, 3648, false); // (default starter) gutenberg wide, relational to large
